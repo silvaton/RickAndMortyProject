cf0c9ec5e7fb842ed0c99426ea05c29accb1f744
silvaton <tdasilva@ext.laligatech.com>
2024-05-09 13:23:30 +0200

    initial setup

---
+++ RickAndMortyApp/RickAndMortyApp/Application/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Ton Silva on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
