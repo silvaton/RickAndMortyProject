b267b336964f107215e833dbef097861a6144331
silvaton <tdasilva@ext.laligatech.com>
2024-05-13 13:17:18 +0200

    Create the mock for RMCharacterModel

--- RickAndMortyApp/RickAndMortyApp/Data/RMCharacterModel.swift
+++ RickAndMortyApp/RickAndMortyApp/Data/RMCharacterModel.swift
@@ -89,3 +89,24 @@ extension RMCharacterLocationModel: Equatable {
             lhs.url == rhs.url
     }
 }
+
+extension RMCharacterModel {
+    static func mock() -> RMCharacterModel {
+        return RMCharacterModel(
+            id: 2,
+            name: "Morty Smit",
+            status: "Alive",
+            species: "Human",
+            type: "",
+            gender: "Male",
+            origin: RMCharacterOriginModel(name: "Earth", url: "https://rickandmortyapi.com/api/location/1"),
+            location: RMCharacterLocationModel(name: "Earth", url: "https://rickandmortyapi.com/api/location/20"),
+            image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+            episode: [
+                "https://rickandmortyapi.com/api/episode/1",
+                "https://rickandmortyapi.com/api/episode/2"],
+            url: "https://rickandmortyapi.com/api/character/2",
+            created: "2017-11-04T18:50:21.651Z"
+        )
+    }
+}
